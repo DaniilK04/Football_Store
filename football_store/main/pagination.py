e2b3594459c1d8b4fa8e44625e082c543223e5c5
Daniil <danya_kozlov_99_99@mail.ru>
2026-02-11 16:34:31 +0300

    Инициализировал репозиторий в git

--- football_store/main/pagination.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.pagination import CursorPagination
-
-class ProductPaginateCursor(CursorPagination):
-
-    page_size = 20  # сколько объектов на страницу
-    ordering = "-created_at"  # поле сортировки
-    page_size_query_param = None  # можно разрешить менять размер
-    max_page_size = None  # максимум объектов
-    cursor_query_param = "cursor"  # имя параметра в URL
